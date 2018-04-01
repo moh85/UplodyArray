1b4222809b7c12ff9296c281c6ec1f18ec293b3c
Mohammad Atwi <mega.web.own@gmail.com>
2018-04-01 19:10:51 +0300

    Update db.php

--- db.php
+++ db.php
@@ -6,7 +6,7 @@ array("2","Hassan","23"),
 
 $website = array("title"=>"Welcome to my website", "slogan"=>"Better web-based systems.");
 
-$any = array(
+$users2 = array(
 array('id'=>1,'age'=>"21",'name'=>"Mohammad Atwi",'country'=>"Lebanon"),
 array('id'=>2,'age'=>"24",'name'=>"Ali Hassan",'country'=>"Syria"),
 array('id'=>3,'age'=>"25",'name'=>"Hussein Kassem",'country'=>"Lebanon"),
