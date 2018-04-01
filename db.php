7ec82399ea9a594d28c612865fcbab9a1b349455
Mohammad Atwi <mega.web.own@gmail.com>
2018-04-01 19:08:05 +0300

    Create db.php

---
+++ db.php
@@ -0,0 +1,13 @@
+$users = array(
+array("0","Mohammad","21"),
+array("1","Ali","25"),
+array("2","Hassan","23"),
+);
+
+$website = array("title"=>"Welcome to my website", "slogan"=>"Better web-based systems.");
+
+$any = array(
+array('id'=>1,'age'=>"21",'name'=>"Mohammad Atwi",'country'=>"Lebanon"),
+array('id'=>2,'age'=>"24",'name'=>"Ali Hassan",'country'=>"Syria"),
+array('id'=>3,'age'=>"25",'name'=>"Hussein Kassem",'country'=>"Lebanon"),
+);
